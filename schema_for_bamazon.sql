581e3675e8109a798feaa5753c762ef3bd7678b9
Danielthweatt <dthweatt192@yahoo.com>
2018-05-04 09:34:28 -0700

    initial file creation (except README)

---
+++ schema_for_bamazon.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price INTEGER(10) NOT NULL,
+stock_quantity INTEGER(10) NOT NULL,
+PRIMARY KEY (item_id)
+);
---
+++ seeds_for_bamazon.sql
@@ -0,0 +1,34 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Naming and Necessity", "Books", 12, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Lord of the Rings", "DVDs", 20, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mind and Cosmos", "Books", 11, 9);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Microwave", "Appliances", 50, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Blender", "Appliances", 30, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Socks", "Clothing", 8, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Camera", "Electronics", 70, 13);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toaster", "Appliances", 25, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ring", "Jewelry", 80, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Jeans", "Clothing", 30, 21);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Star Wars", "Movies", 20, 17);
